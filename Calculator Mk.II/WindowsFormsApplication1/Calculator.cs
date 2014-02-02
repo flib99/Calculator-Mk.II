fed59ff33ed98db02ee9c209d646fe79d7d97ea6
joshwalls <josh@wallsfamily.co.uk>
2014-02-02 21:19:38 +0000

    1.0b1

--- Calculator Mk.II/WindowsFormsApplication1/Calculator.cs
+++ Calculator Mk.II/WindowsFormsApplication1/Calculator.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Claculator : Form
     {
-        public Form1()
+        public Claculator()
         {
             InitializeComponent();
         }
@@ -22,8 +15,6 @@ namespace WindowsFormsApplication1
         private int numberTwo;
         private bool plusOrMinus;
         private int answer;
-
-       
 
         private void btn1_Click(object sender, EventArgs e)    //On button 1 click. Same process for every button.
         {
@@ -123,7 +114,7 @@ namespace WindowsFormsApplication1
                 answer = numberOne - numberTwo;
                 display.Text = Convert.ToString(answer);
             }
-			return;
         }
+
     }
 }
